f6235349de14f1476e066da8d5ae57597ba325cc
Marku <93548824+markumed1@users.noreply.github.com>
2021-12-30 08:57:28 -1000

    Add files via upload

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,8 @@
+<script>
+	function My_First_Function() {
+		var String = "Kiss me, I'm Irish!";
+		var result = String.fontcolor("green");
+		document.getElementById("Irish").innerHTML =
+		result;
+	}
+</script>

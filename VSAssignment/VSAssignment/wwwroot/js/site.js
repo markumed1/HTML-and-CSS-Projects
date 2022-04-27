b33dd9e8d29e22ac7f4fb593d8c5facd98979ffb
Mark Medrano <markumed1@hotmail.com>
2022-04-26 14:56:06 -1000

    Commit for code review

--- VSAssignment/VSAssignment/wwwroot/js/site.js
+++ VSAssignment/VSAssignment/wwwroot/js/site.js
@@ -1,4 +1,17 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿var id = null;
+function myMove() {
+    var elem = document.getElementById("myAnimation");
 
-// Write your Javascript code.
+    var pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 10);
+    function frame() {
+        if (pos == 350) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + 'px';
+            elem.style.left = pos + 'px';
+        }
+    }
+}
